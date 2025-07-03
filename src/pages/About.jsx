1e71dadb0775098c5fbf167931fb0cb05960d914
RonakHirawat <hirawat008ronak@gmail.com>
2025-07-03 16:52:34 +0530

    added new info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -44,7 +44,9 @@ const About = () => {
               <div>
                 <h4 className="font-medium text-gray-700 dark:text-gray-300 text-sm sm:text-base">QwikLabs Developer Club</h4>
                 <p className="text-gray-600 dark:text-gray-400 text-sm sm:text-base">Associate Lead - Corporate Domain</p>
-                <p className="text-xs sm:text-sm text-gray-500 dark:text-gray-400">2023 - Present</p>
+                <p className="text-xs sm:text-sm text-gray-500 dark:text-gray-400">September 2024 - July 2025</p>
+                <p className="text-gray-600 dark:text-gray-400 text-sm sm:text-base">President - Corporate Domain</p>
+                <p className="text-xs sm:text-sm text-gray-500 dark:text-gray-400">July 2025 - Present</p>
               </div>
             </div>
           </div>
